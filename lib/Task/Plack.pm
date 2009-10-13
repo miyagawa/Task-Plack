a00461e938ee93c881f71faca69dfb40321d994a
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-10-13 12:16:57 -0700

    initial commit

---
+++ lib/Task/Plack.pm
@@ -0,0 +1,35 @@
+package Task::Plack;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Task::Plack - Plack bundle
+
+=head1 SYNOPSIS
+
+  cpan> install Task::Plack;
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<http://plackperl.org/>
+
+=cut
