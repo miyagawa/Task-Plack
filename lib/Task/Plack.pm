a88d23f40517eac3747859a63882737f6091b1cf
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2011-01-25 14:26:22 -0800

    Requires Devel::StackTrace::WithLexicals 0.08 to support 'message'

--- lib/Task/Plack.pm
+++ lib/Task/Plack.pm
@@ -17,7 +17,7 @@ sub dependencies {
             [ 'Net::FastCGI' ],
         ],
         'Stacktrace with lexical variables', 0, [
-            [ 'Devel::StackTrace::WithLexicals' ],
+            [ 'Devel::StackTrace::WithLexicals', 0.08 ],
         ],
         'Utility to create IO::Handle-ish objects', 1, [
             [ 'IO::Handle::Util' ],
